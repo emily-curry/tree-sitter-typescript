37210f362c20a824f93334c1dd2d90cba95f8796
Emily Curry <emily@curry.sh>
2021-09-26 14:23:29 -0500

    feat(query): add object literal declaration queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -15,6 +15,24 @@
 (required_parameter (identifier) @variable.parameter)
 (optional_parameter (identifier) @variable.parameter)
 
+; Object Literals
+
+(object) @objectliteral
+
+(object [
+  (pair 
+    key: (property_identifier) @objectliteral.key)
+  (shorthand_property_identifier) @objectliteral.key @objectliteral.key.shorthand
+  (pair 
+    key: (property_identifier) @objectliteral.key @objectliteral.key.function
+    value: [
+      (arrow_function)
+      (function)
+    ])
+  (method_definition 
+    name: (property_identifier) @objectliteral.key @objectliteral.key.method)
+])
+
 ; Keywords
 
 [ "abstract"
